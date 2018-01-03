49d0d7fefaaabab53b1f3fdbaa60e5352149a772
front-end-captain <m17693211019@163.com>
2018-01-03 16:10:21 +0800

    init project
    init project directory and confgi about stylus

---
+++ README.md
@@ -0,0 +1,15 @@
+# music_player
+
+> a music player based on vue、vue-router and vuex
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    hello vue.js
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  @import '~common/stylus/variable'
+  #app
+    color: $color-theme
+</style>
