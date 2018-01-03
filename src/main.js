a82509cfaa9a8bdef4207e2515c529f17a3a43ea
front-end-captain <m17693211019@163.com>
2018-01-03 16:45:41 +0800

    developing header component, named "m-header"

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
+import 'babel-polyfill'
 import Vue from 'vue'
 import App from './App'
+import FastClick from 'fastclick'
 
 import 'common/stylus/index.styl'
+
+// 引入 fastclick 解决移动端点击事件的 300ms 延时问题
+FastClick.attach(document.body)
 
 /* eslint-disable no-new */
 new Vue({
